f0851534e8e8d82411c2d5e470980bed8ba23520
tahs88 <tahs88@localhost>
2012-05-19 17:04:16 +1000

    Using MatrixLightLib for some matrix operations. Added some of the code required for the Kalman Filter.
    added MatrixLiteLib/include/MatrixLiteLib.h
    added MatrixLiteLib/src/MatrixLiteLib.c
    changed firmware/makefile
    changed firmware/src/tilt.c

--- firmware/src/tilt.c
+++ firmware/src/tilt.c
@@ -33,6 +33,8 @@
 
 #include <math.h>
 
+#include "MatrixLiteLib.h"
+
 #include "sensors.h"
 
 #define GRN_LED_PRT 2
@@ -62,16 +64,47 @@ void setup_tilt(void) {
 }
 
 typedef float tfloat;
-void integrateOneStep(tfloat dt, tfloat* w, tfloat* oldPhi, tfloat* newPhi) {
-  tfloat sinphix = sin(oldPhi[0]);
-  tfloat cosphix = cos(oldPhi[0]);
-  tfloat cosphiy = cos(oldPhi[1]);
-  tfloat tanphiy = tan(oldPhi[1]);
 
-  newPhi[0] = oldPhi[0] + (w[0] + (w[1]*sinphix + w[2]*cosphix)*tanphiy)*dt;
-  newPhi[1] = oldPhi[1] + (w[1]*cosphix - w[2]*sinphix)*dt;
-  newPhi[2] = oldPhi[2] + (w[1]*sinphix + w[2]*cosphix)*dt/cosphiy;
+void processModel( tfloat* X, tfloat* w, tfloat* Xdot) {
+  tfloat sinphix = sin(X[0]);
+  tfloat cosphix = cos(X[0]);
+  tfloat cosphiy = cos(X[1]);
+  tfloat tanphiy = tan(X[1]);
+
+  Xdot[0] = (w[0] + (w[1]*sinphix + w[2]*cosphix)*tanphiy);
+  Xdot[1] =  w[1]*cosphix - w[2]*sinphix;
+  Xdot[2] = (w[1]*sinphix + w[2]*cosphix)/cosphiy;
 }
+
+void processModelJacobian( tfloat* X, tfloat* w, LPMATRIX J, tfloat step ) {
+  tfloat tmp;
+  tfloat Xtmp[3];
+  tfloat dX[3];
+  int i,j;
+  for(i=0; i<3; i++) {
+    for(j=0; j<3; j++) {
+      memcpy(Xtmp,X,3*sizeof(tfloat));
+      Xtmp[i]+=step;
+      processModel(Xtmp,w,dX);
+      tmp=dX[j];
+      processModel(X,w,dX);
+      tmp-=dX[j];
+      MatrixSet(J,i,j,tmp/step);
+    }
+  }
+
+}
+
+void integrateOneStep(tfloat dt, tfloat* w, tfloat* oldPhi, tfloat* newPhi) {
+
+  processModel(oldPhi,w,newPhi);
+
+  newPhi[0] = oldPhi[0] + newPhi[0]*dt;
+  newPhi[1] = oldPhi[1] + newPhi[1]*dt;
+  newPhi[2] = oldPhi[2] + newPhi[2]*dt;
+}
+
+
 
 int main(void){
     setup_tilt();
@@ -89,24 +122,20 @@ int main(void){
     int32_t accy=0; // Acc Y Out
     int32_t accz=0; // Acc Z Out
     
-    int32_t magx=0; // Mag X Out
-    int32_t magy=0; // Mag Y Out
-    int32_t magz=0; // Mag Z Out
-    
     float AccXDeg;
     float AccYDeg;
-    float MagBearing;
-    float MagTheta;
 
-    tfloat oldPhi[3];
+    tfloat X[3];
     tfloat newPhi[3];
     tfloat w[3];
     tfloat dt;
     sc_time_t t_old = sc_get_timer();
 
-    oldPhi[0] = 0;
-    oldPhi[1] = 0;
-    oldPhi[2] = 0;
+    X[0] = 0;
+    X[1] = 0;
+    X[2] = 0;
+
+    LPMATRIX J = CreateMatrix(3,3);
 
 
     while(1){
@@ -117,26 +146,22 @@ int main(void){
 	   it into degrees per second. */
 	w[0]*=Pi/180.0;
 	w[1]*=Pi/180.0;
-	w[2]*=Pi/180.0;        
+	w[2]*=Pi/180.0;
 
-
-        readacc(2, &accx, &accy, &accz, &acct);
+	readacc(2, &accx, &accy, &accz, &acct);
         acc2deg(accx, accy, accz, &AccXDeg, &AccYDeg); 
 
 	sc_time_t t = sc_get_timer();
 	dt = (tfloat)(t - t_old);
 	t_old = t;
 
-	integrateOneStep(dt/1000.0,w,oldPhi,newPhi);
+	
+	// Prediction from old best guess
+	integrateOneStep(dt/1000.0,w,X,newPhi);
+	
+	processModelJacobian(X,w,J,0.0);
 
-	//testloop+=0.001;
-
-        UART_printf("GyroInt:%3.3f,%3.3f,%3.3f\n\r",newPhi[0],newPhi[1],newPhi[2]);
-	//UART_printf("GyroOmega:%3.3f, %3.3f, %3.3f\n\r", w[0], w[1], w[2]);
-	//UART_printf("dt:%3.3f\n\r", dt/1000);
-	UART_printf("AccAngle:%3.3f,%3.3f\n\r", AccXDeg, AccYDeg);
-	//UART_printf("What's Going on?: f:%f, 3f5:%3.5f, loop:%3.5f\n\r", newPhi[0], newPhi[0], testloop);
-	memcpy(oldPhi,newPhi,3*sizeof(tfloat));
+	memcpy(X,newPhi,3*sizeof(tfloat));
 
     	GPIO_SetValue(YEL_LED_PRT, YEL_LED_BIT,1);
 	scandal_delay(100);
